9f41c0c4c3bef399dff081ae12caf5255b3d05b4
Lysandro <lysandro.carioca@hp.com>
2021-01-25 21:50:30 +0000

    learn golang with tdd

--- learn-go-with-tests/arrays-slices/sum_test.go
+++ learn-go-with-tests/arrays-slices/sum_test.go
@@ -1,14 +1,43 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	compare := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
 	}
+
+	t.Run("collection of  any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		compare(t, got, want, numbers)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	compare := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		compare(t, got, want)
+	})
+
+	t.Run("safety sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		compare(t, got, want)
+	})
 }
---
+++ learn-go-with-tests/iteration/iteration_test.go
@@ -0,0 +1,18 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", b.N)
+	}
+}
