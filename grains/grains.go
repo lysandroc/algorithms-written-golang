8d9c0c21aefb3b79b71f8d336d063ec60b776858
Lysandro Carioca <lysandroc@gmail.com>
2021-01-27 20:35:01 -0300

    algorithm improvement

--- grains/grains.go
+++ grains/grains.go
@@ -2,20 +2,15 @@ package grains
 
 import (
 	"fmt"
-	"math"
 )
 
 func Square(n int) (uint64, error) {
 	if n < 1 || n > 64 {
-		return 0, fmt.Errorf("error")
+		return 0, fmt.Errorf("Invalid interval")
 	}
-	return uint64(math.Exp2(float64(n - 1))), nil
+	return 1 << (n - 1), nil
 }
 
 func Total() uint64 {
-	var sum uint64
-	for i := 0; i < 64; i++ {
-		sum += uint64(math.Exp2(float64(i)))
-	}
-	return sum
+	return (1 << 64) - 1
 }
